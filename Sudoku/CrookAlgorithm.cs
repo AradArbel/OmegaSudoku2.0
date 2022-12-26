157f186984c0961813048a71c80d270b91945c61
Arad Arbel <aradarbel777@gmail.com>
2022-12-26 22:19:38 +0200

    add crook algorithem which remove at the beginning all unpossible values from each cell values list

---
+++ Sudoku/CrookAlgorithm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace Sudoku
+{
+    public class CrookAlgorithm
+    {
+        // Remove values that are already used in the row, column, and block
+        public void removeUnpossibleValues(SudokuCell cell, SudokuBoard board)
+        {
+            int row = cell.Row;
+            int col = cell.Col;
+
+            for (int cellIndex = 0; cellIndex < Constants.maxCellValue; cellIndex++)
+            {
+                cell.PossibleValues.Remove(board.Board[row, cellIndex]);
+                cell.PossibleValues.Remove(board.Board[cellIndex, col]);
+            }
+
+            int blockRow = row / (int)Math.Sqrt(Constants.maxCellValue);
+            int blockCol = col / (int)Math.Sqrt(Constants.maxCellValue);
+
+            for (int i = blockRow * (int)Math.Sqrt(Constants.maxCellValue); i < blockRow * (int)Math.Sqrt(Constants.maxCellValue) + (int)Math.Sqrt(Constants.maxCellValue); i++)
+            {
+                for (int j = blockCol * (int)Math.Sqrt(Constants.maxCellValue); j < blockCol * (int)Math.Sqrt(Constants.maxCellValue) + (int)Math.Sqrt(Constants.maxCellValue); j++)
+                {
+                    cell.PossibleValues.Remove(board.Board[i, j]);
+                }
+            }
+
+            //set possibleValues;
+        }
+
+    }
+    
+}
