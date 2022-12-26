6d8d5c5fcfef1cf37de012b2de9fe9c717c34139
Arad Arbel <aradarbel777@gmail.com>
2022-12-26 14:41:52 +0200

    first time working on the solution algorithm

---
+++ Sudoku/SudokuSolver.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace Sudoku
+{
+    public class SudokuSolver
+    {
+        //Check whether value is already exist in col or not
+        bool isExistInCol(SudokuCell cell, SudokuBoard board, int num)
+        {
+            int col = cell.Col;
+            for (int row = 0; row < Constants.boardRange; row++)
+                if (board.Board[row, col].Value == num)
+                    return true;
+            return false;
+        }
+
+        //Check whether value is already exist in row or not
+        bool isExistInRow(SudokuCell cell, SudokuBoard board, int num)
+        {
+            int row = cell.Row;
+            for (int col = 0; col < Constants.boardRange; col++)
+                if (board.Board[row, col].Value == num)
+                    return true;
+            return false;
+        }
+
+        //Check whether value is already exist in box or not
+        bool isExistInBox(int boxStartRow, int boxStartCol, SudokuBoard board, int num)
+        {
+            for (int row = 0; row < Constants.boxRange; row++)
+                for (int col = 0; col < Constants.boxRange; col++)
+                    if (board.Board[row + boxStartRow, col + boxStartCol].Value == num)
+                        return true;
+            return false;
+        }
+
+        //get empty location and update row and column
+        bool findEmptyPlace(SudokuBoard board)
+        {
+            int row, col;
+            for (row = 0; row < Constants.boardRange; row++)
+                for (col = 0; col < Constants.boardRange; col++)
+                    if (board.Board[row, col].Value == 0) //marked with 0 is empty
+                        return true;
+            return false;
+        }
+
+        //Check if item is not found in col, row and current box
+        bool isValidPlace(SudokuCell cell, SudokuBoard board, int num)
+        {
+
+            return !isExistInRow(cell, board, num)
+                && !isExistInCol(cell, board, num)
+                && !isExistInBox((int)(cell.Row - cell.Row % Constants.boxRange), (int)(cell.Col - cell.Col % Constants.boxRange), board, num);
+        }
+
+        // Solve the sudoku in recursive backtracking method
+        bool solveSudoku(SudokuBoard board, int row, int col)
+        {
+            if (!findEmptyPlace(board))
+                return true; //when all places are filled
+            for (int num = Constants.minCellValue; num <= Constants.maxCellValue; num++)
+            {
+                if (isValidPlace(board.Board[row, col], board, num))
+                { //check validation, if yes, put the number in the grid
+                    board.Board[row, col].SetValue(num);
+                    if (solveSudoku(board, ++row, ++col))     //recursively go for other rooms in the grid
+                        return true;
+                    board.Board[row, col].SetValue(0);    //turn to unassigned space when conditions are not satisfied
+                }
+            }
+            return false;
+        }
+    }
+}
